fa7a63497bf24b87b22a457ef0abf74af004688c
ZukoA <azerinazuko@gmail.com>
2021-03-11 23:25:38 +0100

    Day04

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+function main (){
+    var myRes = 75;
+    myRes++; // Ovdje code
+    return myRes;
+}
+
+console.log(main()); 
+module.exports = main;
---
+++ ex03/src/index.js
@@ -0,0 +1,8 @@
+function main (){
+    var myRes = 16;
+    myRes--; // Ovdje code
+    return myRes;
+}
+
+console.log(main()); 
+module.exports = main;
